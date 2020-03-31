4a7f62f3baeb35e91fe5acdafd83bf056c062ff8
LeedWover <aresone.gaben@gmail.com>
2020-03-31 10:18:30 +0200

    .env

--- server/data.js
+++ server/data.js
@@ -2,14 +2,34 @@ const fetch = require('node-fetch');
 const geoCoder = require('./geocoder');
 
 API_URL = process.env.CASES_API_URL;
-
+console.log(API_URL)
 async function fetchData(url) {
   const res = await fetch(url);
   const data = await res.json();
   return data;
-};
+}
 
 async function prepareJson() {
+  let raw = {};
+  try {
+    raw = await fetchData(API_URL);
+    let keys = Object.keys(raw);
+    let array = [];
+    console.log('run');
+    for (let i = 0; i < 1; i++) {
+      const key = keys[i];
+      const lastItem = raw[key].pop();
+      lastItem.name = key;
+      array.push(lastItem);
+      console.log(array);
+    }
+    return array;
+  } catch (error) {
+    console.error(error);
+  }
+}
+prepareJson();
+/* async function prepareJson() {
   let raw = await fetchData(API_URL);
   let keys = Object.keys(raw);
   let array = [];
@@ -20,8 +40,8 @@ async function prepareJson() {
   })
   return array;
 
-}
+} */
 
 module.exports = {
   prepareJson
-}
+};
