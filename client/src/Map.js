767b111fa28a0c0a05cc4e16dc6b6fc086a679d0
LeedWover <aresone.gaben@gmail.com>
2020-03-31 23:30:42 +0200

    show details

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   withScriptjs,
   withGoogleMap,
@@ -9,7 +9,12 @@ import {
 
 const MyMapComponent = withScriptjs(
   withGoogleMap(({isMarkerShown, cases}) => {
+    const [details, setDetails] = useState(null)
+    const showDetails = (item) => {
+      setDetails(item)
+    }
     return (
+      
       <div style={{maxHeight: '100vh', overflowX: 'hidden'}}>
       <GoogleMap googleMapURL={process.env.REACT_APP_MAP_URL} defaultZoom={5} defaultCenter={{ lat: 53.0, lng: 9.0 }}>
         {cases ? cases.map(item => (
@@ -19,12 +24,23 @@ const MyMapComponent = withScriptjs(
             lat: item.geo.lat,
             lng: item.geo.lng
           }}
-          radius={item.confirmed >= 10000 ? item.confirmed * 3.6 : item.confirmed * 25}
+          radius={item.confirmed >= 7000 ? item.confirmed * 2 : item.confirmed * 27}
+          onClick={() => showDetails(item)}
         />
         )) : null}
         
         {isMarkerShown && <Marker position={{ lat: 53.0, lng: 9.0 }} />}
       </GoogleMap>
+      <div style={{background: '#fff', opacity: '0.4', position: 'absolute', top: '0', left: '50%'}}>
+          {details ? (
+            <>
+              <span style={{fontSize: '5em'}}>{details.name}</span><br/>
+              <span style={{fontSize: '5em'}}>{details.confirmed}</span><br/>
+            </>
+          
+          ) : null
+        }
+      </div>
       </div>
     );
   })
