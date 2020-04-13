f460dd9e26c3814d3fe393f191960f2448e54efb
LeedWover <aresone.gaben@gmail.com>
2020-04-13 12:08:42 +0200

    _

--- client/src/Countries.js
+++ client/src/Countries.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
-import InfoItem from './Country';
+import Countries from './Country';
 
 function InfoContainer({ cases }) {
   return (
     <div className="info_container" style={{ maxHeight: '100vh', overflowX: 'hidden' }}>
       <ul className="list-group info_list">
         {cases
-          ? cases.map(item => (
-              <InfoItem key={item._id} name={item.name} confirmed={item.confirmed} deaths={item.deaths} recovered={item.recovered} />
+          ? cases.sort((a, b) => b.confirmed - a.confirmed).map(item => (
+              <Countries key={item._id} name={item.name} confirmed={item.confirmed} deaths={item.deaths} recovered={item.recovered} />
             ))
           : 'Loading...'}
       </ul>
